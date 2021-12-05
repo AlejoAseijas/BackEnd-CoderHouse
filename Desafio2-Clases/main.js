40f68a81a957ebb09d35cd00011e39f941cf4801
Alejo Agustín Martinez Aseijas <martinezalejo123a@gmail.com>
2021-12-05 16:50:05 -0300

    fix errors desafio clase2

--- Desafio2-Clases/main.js
+++ Desafio2-Clases/main.js
@@ -1,84 +1,96 @@
-const fs = require("fs");
-
+class Producto {
+  constructor(title, price, thumbnail) {
+    (this.title = title), (this.price = price), (this.thumbnail = thumbnail);
+  }
+}
 class Contenedor {
-  constructor(fileName) {
-    this.fileName = fileName;
+  fs = require("fs");
+
+  constructor(file) {
+    this.file = file;
+    this.codif = "utf-8";
   }
 
-  save(dataPart) {
-    dataPart.map((data, index) => {
-      data.id = index + 1;
-      fs.promises
-        .writeFile(`./${this.fileName}.txt`, JSON.stringify(dataPart))
-        .then(() => {
-          console.log(data.id);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
+  async save(dataProduct) {
+    const data = await this.getAll();
+    dataProduct.id = data.length + 1;
+    data.push(dataProduct);
+    try {
+      await this.fs.promises.writeFile(
+        this.file,
+        JSON.stringify(data, null, "\t")
+      );
+    } catch (error) {
+      console.log("el archivo no se pudo guardar", error);
+    }
   }
-  getById(id) {
-    fs.promises
-      .readFile(`./${this.fileName}.txt`, "utf-8")
-      .then((data) => {
-        let dataToFind = JSON.parse(data);
-        let productToShow = dataToFind.find((prod) => {
-          return prod.id === id;
-        });
-        productToShow === undefined
-          ? console.log("No hay ningun producto con ese id")
-          : console.log(productToShow);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+  async getAll() {
+    try {
+      let data = await this.fs.promises.readFile(`./${this.file}`, this.codif);
+      return JSON.parse(data);
+    } catch {
+      return [];
+    }
   }
-  getAll() {
-    fs.promises
-      .readFile(`./${this.fileName}.txt`, "utf-8")
-      .then((data) => {
-        console.log(JSON.parse(data));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+  async getById(id) {
+    try {
+      let dat = await this.fs.promises.readFile(`./${this.file}`, this.codif);
+      let datJson = JSON.parse(dat);
+      let product = datJson.find((dat) => dat.id === id);
+      return product;
+    } catch (err) {
+      return err;
+    }
   }
-  deleteById(id) {
-    fs.promises
-      .readFile(`./${this.fileName}.txt`, "utf-8")
-      .then((data) => {
-        let dataToFilter = JSON.parse(data);
-        let newData = dataToFilter.filter((prod) => prod.id != id);
-        fs.promises.writeFile(
-          `./${this.fileName}.txt`,
-          JSON.stringify(newData)
-        );
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+  async delete() {
+    try {
+      await this.fs.promises.unlink(`./${this.file}`);
+    } catch (error) {
+      console.log("no se pudo borrar el archivo", error);
+    }
   }
-  deleteAll() {
-    fs.promises.truncate(`./${this.fileName}.txt`);
+
+  async deleteById(id) {
+    try {
+      let dat = await this.fs.promises.readFile(`./${this.file}`, this.codif);
+      let datJson = JSON.parse(dat);
+      let newProductList = datJson.filter((dat) => dat.id != id);
+      await this.fs.promises.writeFile(
+        `./${this.file}`,
+        JSON.stringify(newProductList)
+      );
+    } catch (error) {
+      console.log("no se pudo borrar el archivo", error);
+    }
   }
 }
 
-const product = new Contenedor("part");
-product.save([
-  {
-    title: "Rv7 ruder",
-    price: 500,
-    thumbnail:
-      "https://www.rv7-factory.com/wp-content/uploads/2011/10/2011-10-01-17.30.57.jpg",
-  },
-]);
+const test = async () => {
+  const itemNuevo1 = new Producto(
+    "Teclado",
+    4500.55,
+    "https://m.media-amazon.com/images/I/81vl4qsTwTL._AC_SY450_.jpg"
+  );
+  const itemNuevo2 = new Producto(
+    "Mouse",
+    2500.2,
+    "https://www.logitech.com/content/dam/logitech/en/products/mice/m171/gallery/m171-gallery-grey-1.png"
+  );
+  const itemNuevo3 = new Producto(
+    "Auriculares",
+    9500.22,
+    "https://co.jbl.com/dw/image/v2/AAUJ_PRD/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw5b7fa15b/JBL_Quantum_400_Product%20Image_Hero%2002.png?sw=537&sfrm=png"
+  );
 
-// product.getById(1);
-// product.getAll();
-// product.deleteById(1);
+  const data = new Contenedor("productos.txt");
 
-// product.deleteAll();
-product.deleteAll();
+  //   await data.guardar(itemNuevo1);
+  //   await data.guardar(itemNuevo2);
+  //   await data.guardar(itemNuevo3);
+  //   await data.deleteById(2);
+};
 
-// console.log(product.getById(2));
+test();
