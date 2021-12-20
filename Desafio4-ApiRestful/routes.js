b58e19886f8a8750e6c42c2e4285a90dfb1edc72
Alejo Agustín Martinez Aseijas <martinezalejo123a@gmail.com>
2021-12-20 16:03:04 -0300

    fixs

--- Desafio4-ApiRestful/routes.js
+++ Desafio4-ApiRestful/routes.js
@@ -7,26 +7,28 @@ const router = Router();
 // Router
 router.get("/productos", async (req, res) => {
   const resData = await productosReq.get();
-  res.send(resData);
+  if (resData) {
+    res.json(resData);
+  }
 });
 
 router.get("/productos/:id", async (req, res) => {
   const resData = await productosReq.getById(req.params.id);
-  res.send(resData);
+  res.json(resData);
 });
 
 router.post("/productos", async (req, res) => {
   const resData = await productosReq.post(req.body);
-  res.send(resData);
+  res.json(resData);
 });
 router.put("/productos/:id", async (req, res) => {
   const resData = await productosReq.put(req.params.id, req.body);
-  res.send(resData);
+  res.json(resData);
 });
 
 router.delete("/productos/:id", async (req, res) => {
   const resData = await productosReq.delete(req.params.id);
-  res.send(resData);
+  res.json(resData);
 });
 
 // Fin router
