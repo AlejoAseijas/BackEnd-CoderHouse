70c06848d66e3f03a8b44dc5d0a0d572df04c3fa
Alejo Aseijas <alejoagustinaseijas@gmail.com>
2022-01-13 13:38:42 -0300

    fix

--- Desafio4-ApiRestful/index.js
+++ Desafio4-ApiRestful/index.js
@@ -1,7 +1,9 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = 8080;
 const router = require("./routes");
+//const { engine } = require("express-handlebars");
 
 const server = app.listen(PORT, () => {
   console.log(`Server on port ${server.address().port}`);
@@ -10,6 +12,22 @@ const server = app.listen(PORT, () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// app.engine(
+//   "handlebars",
+//   engine({
+//     extname: "hbs",
+//     defaultLayout: "main.hbs",
+//     layoutsDir: path.resolve(__dirname, "./views/layouts"),
+//     partialsDir: path.resolve(__dirname, "./views/partials"),
+//   })
+// );
+
+//sapp.set("views", "./views");
+//app.set("view engine", "handlebars");
+//app.set("view engine", "pug"); //pug
+app.set("view engine", "ejs"); //ejs
+
 app.use("/api", router.routes);
 
 server.on("error", (error) => {
